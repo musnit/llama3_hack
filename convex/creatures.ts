0ea882d58dd853bb46a475bd32da8290b28dd44a
Ramon Iglesias <rdit@stanford.edu>
2024-05-11 14:59:16 -0700

    merging

--- convex/creatures.ts
+++ convex/creatures.ts
@@ -1,4 +1,5 @@
 import { query, action, mutation } from "./_generated/server";
+import { v } from "convex/values";
 
 export const generateUploadUrl = mutation(async (ctx) => {
     return await ctx.storage.generateUploadUrl();
@@ -6,7 +7,8 @@ export const generateUploadUrl = mutation(async (ctx) => {
 
 export const createCreature = action({
   args: {
-
+    storageId: v.id("_storage"),
+    author: v.optional(v.string()),
   },
   handler: async (ctx) => {
     return await ctx.db.query("tasks").collect();
