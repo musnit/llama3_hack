316ea3335105b2074c45f16b4ba5ef82102bc711
musnit <musnit@gmail.com>
2024-05-11 15:06:34 -0700

    add prettier and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import { Tldraw } from 'tldraw'
-import './App.css'
+import { Tldraw } from "tldraw";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div style={{ position: 'fixed', inset: 0 }}>
-    <Tldraw />
-  </div>
-)
+     <div style={{ position: "fixed", inset: 0 }}>
+       <Tldraw />
+    </div>
+  );
 }
 
-export default App
+export default App;
